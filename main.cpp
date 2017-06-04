3b873c91a9793d68362cdc002001b278160cab55
andrewestronick <andy.estronick@hotmail.com>
2017-06-03 21:33:18 -0700

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <windows.h>
 
+#define IMAGE_SIZE 256
 
 using namespace std;
 
@@ -8,49 +9,51 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
 
 int main()
 {
+	HWND hwnd = GetConsoleWindow();
+	HINSTANCE hInstance = (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE);
 
-#pragma region _REGION_A_ - get HWND of console
-
-	char t[500];
-	GetConsoleTitleA(t, 500);
-	HWND hwndConsole = FindWindowA(NULL, t);
-	MoveWindow(hwndConsole, 20, 20, 500, 500, true);
-
-#pragma endregion
-
-#pragma region _REGION_B - get HINSTANCE and create a window!
-
-	HINSTANCE hInstance = (HINSTANCE)GetWindowLong(hwndConsole, GWL_HINSTANCE);
+	WNDCLASS  window;
+	window.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	window.hCursor = LoadCursor(hInstance, IDC_ARROW);
+	window.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	window.hInstance - hInstance;
+	window.lpfnWndProc = WndProc;
+	window.lpszClassName = TEXT("picture window");
+	window.style = CS_HREDRAW | CS_VREDRAW;
 
 	WNDCLASS wc = { 0 };
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
 	wc.hCursor = LoadCursor(hInstance, IDC_ARROW);
 	wc.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
 	wc.hInstance = hInstance;
 	wc.lpfnWndProc = WndProc;
-	wc.lpszClassName = TEXT("Andrew");
+	wc.lpszClassName = TEXT("myWindow");
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 
 	if (!RegisterClass(&wc))
-	{
-		cout << "Problem with your WNDCLASS, foo." << endl;
-		return 1;
-	}
+		exit(-1);
 
-	HWND hwndWindow = CreateWindow(TEXT("Andrew"), TEXT("the window"), WS_OVERLAPPEDWINDOW, 520, 20, 300, 300, NULL, NULL, hInstance, NULL);
+	HWND hwndWindow = CreateWindow(TEXT("myWindow"), TEXT("the window"), WS_OVERLAPPEDWINDOW, 10, 10, IMAGE_SIZE, IMAGE_SIZE, NULL, NULL, hInstance, NULL);
 
 	ShowWindow(hwndWindow, SW_SHOWNORMAL);
 	UpdateWindow(hwndWindow);
-#pragma endregion
+	HDC hdc = GetDC(hwndWindow);
 
-#pragma region _REGION_C - message loop
+	// Draw some ranndom dots
+
+	for (int i = 0; i < 1000; i++)
+	{
+		int x = rand() % IMAGE_SIZE;
+		int y = rand() % IMAGE_SIZE;
+		SetPixel(hdc, x, y, RGB(255, 255, 255));
+	}
+
 	MSG msg;
 	while (GetMessage(&msg, hwndWindow, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-#pragma endregion
 
 	return msg.wParam;
 }
@@ -59,29 +62,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 {
 	switch (message)
 	{
-	case WM_CREATE:
-		cout << "WINDOW SPEAKING TO YOU THROUGH THE CONSOLE." << endl;
-		cout << "I'm alive!" << endl << endl;
-		Beep(40, 40);
-		return 0;
-		break;
-
-	case WM_PAINT:
-	{
-		cout << "PAINT EVENT!  time to repaint!!" << endl;
-		PAINTSTRUCT ps;
-		HDC hdc;
-		BeginPaint(hwnd, &ps);
-		EndPaint(hwnd, &ps);
-		Beep(40, 40);
-		return 0;
-	}
-	break;
 
 	case WM_DESTROY:    // killing the window
-		cout << "NOOO!!  I . . . shall . . . return !!" << endl;
+
 		PostQuitMessage(0);
-		return 0;
+		exit(0);
 		break;
 	}
 
